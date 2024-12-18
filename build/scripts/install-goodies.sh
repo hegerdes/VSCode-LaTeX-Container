133523215ddba9904feb7718415d2255cd54c3fa
Henrik Gerdes <hegerdes@outlook.de>
2024-12-18 22:47:05 +0100

    deps: support ubuntu noble & drop focal
    Signed-off-by: Henrik Gerdes <hegerdes@outlook.de>

--- build/scripts/install-goodies.sh
+++ build/scripts/install-goodies.sh
@@ -9,7 +9,7 @@
 #
 # Syntax: ./common-debian.sh [install zsh flag] [username] [user UID] [user GID] [upgrade packages flag] [install Oh My Zsh! flag] [Add non-free packages]
 
-set -e
+set -ex
 
 INSTALL_ZSH=${1:-"true"}
 USERNAME=${2:-"automatic"}
@@ -30,7 +30,7 @@ test -f $APT_SOURCES_LIST || touch $APT_SOURCES_LIST
 
 # Ensure that login shells get the correct path if the user updated the PATH using ENV.
 rm -f /etc/profile.d/00-restore-env.sh
-echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\$PATH}" > /etc/profile.d/00-restore-env.sh
+echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\$PATH}" >/etc/profile.d/00-restore-env.sh
 chmod +x /etc/profile.d/00-restore-env.sh
 
 # If in automatic mode, determine if a user already exists, if not use vscode
@@ -38,7 +38,7 @@ if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
     USERNAME=""
     POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
     for CURRENT_USER in ${POSSIBLE_USERS[@]}; do
-        if id -u ${CURRENT_USER} > /dev/null 2>&1; then
+        if id -u ${CURRENT_USER} >/dev/null 2>&1; then
             USERNAME=${CURRENT_USER}
             break
         fi
@@ -64,8 +64,7 @@ fi
 export DEBIAN_FRONTEND=noninteractive
 
 # Function to call apt-get if needed
-apt-get-update-if-needed()
-{
+apt-get-update-if-needed() {
     if [ ! -d "/var/lib/apt/lists" ] || [ "$(ls /var/lib/apt/lists/ | wc -l)" = "0" ]; then
         echo "Running apt-get update..."
         apt-get update
@@ -130,10 +129,10 @@ if [ "${PACKAGES_ALREADY_INSTALLED}" != "true" ]; then
     else
         apt-get-update-if-needed
     fi
-    if [ "${CODENAME}" = "bookworm" ] || [ "${CODENAME}" = "jammy" ]; then
-        PACKAGE_LIST="${PACKAGE_LIST} liblttng-ust1"
-    else
+    if [ "${CODENAME}" = "bullseye" ]; then
         PACKAGE_LIST="${PACKAGE_LIST} liblttng-ust0"
+    else
+        PACKAGE_LIST="${PACKAGE_LIST} liblttng-ust1"
     fi
 
     # Install libssl1.1 if available
@@ -154,7 +153,7 @@ if [ "${PACKAGES_ALREADY_INSTALLED}" != "true" ]; then
     fi
 
     echo "Packages to verify are installed: ${PACKAGE_LIST}"
-    apt-get -y install --no-install-recommends ${PACKAGE_LIST} 2> >( grep -v 'debconf: delaying package configuration, since apt-utils is not installed' >&2 )
+    apt-get -y install --no-install-recommends ${PACKAGE_LIST} 2> >(grep -v 'debconf: delaying package configuration, since apt-utils is not installed' >&2)
 
     PACKAGES_ALREADY_INSTALLED="true"
 fi
@@ -168,14 +167,14 @@ fi
 
 # Ensure at least the en_US.UTF-8 UTF-8 locale is available.
 # Common need for both applications and things like the agnoster ZSH theme.
-if [ "${LOCALE_ALREADY_SET}" != "true" ] && ! grep -o -E '^\s*en_US.UTF-8\s+UTF-8' /etc/locale.gen > /dev/null; then
-    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+if [ "${LOCALE_ALREADY_SET}" != "true" ] && ! grep -o -E '^\s*en_US.UTF-8\s+UTF-8' /etc/locale.gen >/dev/null; then
+    echo "en_US.UTF-8 UTF-8" >>/etc/locale.gen
     locale-gen
     LOCALE_ALREADY_SET="true"
 fi
 
 # Create or update a non-root user to match UID/GID.
-if id -u ${USERNAME} > /dev/null 2>&1; then
+if id -u ${USERNAME} >/dev/null 2>&1; then
     # User exists, update if needed
     if [ "${USER_GID}" != "automatic" ] && [ "$USER_GID" != "$(id -G $USERNAME)" ]; then
         groupmod --gid $USER_GID $USERNAME
@@ -200,7 +199,7 @@ fi
 
 # Add add sudo support for non-root user
 if [ "${USERNAME}" != "root" ] && [ "${EXISTING_NON_ROOT_USER}" != "${USERNAME}" ]; then
-    echo $USERNAME ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME
+    echo $USERNAME ALL=\(root\) NOPASSWD:ALL >/etc/sudoers.d/$USERNAME
     chmod 0440 /etc/sudoers.d/$USERNAME
     EXISTING_NON_ROOT_USER="${USERNAME}"
 fi
@@ -213,17 +212,18 @@ else
 fi
 
 # Restore user .bashrc defaults from skeleton file if it doesn't exist or is empty
-if [ ! -f "${USER_RC_PATH}/.bashrc" ] || [ ! -s "${USER_RC_PATH}/.bashrc" ] ; then
-    cp  /etc/skel/.bashrc "${USER_RC_PATH}/.bashrc"
+if [ ! -f "${USER_RC_PATH}/.bashrc" ] || [ ! -s "${USER_RC_PATH}/.bashrc" ]; then
+    cp /etc/skel/.bashrc "${USER_RC_PATH}/.bashrc"
 fi
 
 # Restore user .profile defaults from skeleton file if it doesn't exist or is empty
-if  [ ! -f "${USER_RC_PATH}/.profile" ] || [ ! -s "${USER_RC_PATH}/.profile" ] ; then
-    cp  /etc/skel/.profile "${USER_RC_PATH}/.profile"
+if [ ! -f "${USER_RC_PATH}/.profile" ] || [ ! -s "${USER_RC_PATH}/.profile" ]; then
+    cp /etc/skel/.profile "${USER_RC_PATH}/.profile"
 fi
 
 # .bashrc/.zshrc snippet
-RC_SNIPPET="$(cat << 'EOF'
+RC_SNIPPET="$(
+    cat <<'EOF'
 
 if [ -z "${USER}" ]; then export USER=$(whoami); fi
 if [[ "${PATH}" != *"$HOME/.local/bin"* ]]; then export PATH="${PATH}:$HOME/.local/bin"; fi
@@ -253,7 +253,7 @@ EOF
 )"
 
 # code shim, it fallbacks to code-insiders if code is not available
-cat << 'EOF' > /usr/local/bin/code
+cat <<'EOF' >/usr/local/bin/code
 #!/bin/sh
 
 get_in_path_except_current() {
@@ -274,7 +274,7 @@ EOF
 chmod +x /usr/local/bin/code
 
 # systemctl shim - tells people to use 'service' if systemd is not running
-cat << 'EOF' > /usr/local/bin/systemctl
+cat <<'EOF' >/usr/local/bin/systemctl
 #!/bin/sh
 set -e
 if [ -d "/run/systemd/system" ]; then
@@ -286,8 +286,9 @@ EOF
 chmod +x /usr/local/bin/systemctl
 
 # Codespaces bash and OMZ themes - partly inspired by https://github.com/ohmyzsh/ohmyzsh/blob/master/themes/robbyrussell.zsh-theme
-CODESPACES_BASH="$(cat \
-<<'EOF'
+CODESPACES_BASH="$(
+    cat \
+        <<'EOF'
 
 # Codespaces bash prompt theme
 __bash_prompt() {
@@ -313,8 +314,9 @@ __bash_prompt
 EOF
 )"
 
-CODESPACES_ZSH="$(cat \
-<<'EOF'
+CODESPACES_ZSH="$(
+    cat \
+        <<'EOF'
 # Codespaces zsh prompt theme
 __zsh_prompt() {
     local prompt_username
@@ -338,16 +340,18 @@ EOF
 )"
 
 # Add notice that Oh My Bash! has been removed from images and how to provide information on how to install manually
-OMB_README="$(cat \
-<<'EOF'
+OMB_README="$(
+    cat \
+        <<'EOF'
 "Oh My Bash!" has been removed from this image in favor of a simple shell prompt. If you
 still wish to use it, remove "~/.oh-my-bash" and install it from: https://github.com/ohmybash/oh-my-bash
 You may also want to consider "Bash-it" as an alternative: https://github.com/bash-it/bash-it
 See here for infomation on adding it to your image or dotfiles: https://aka.ms/codespaces/omb-remove
 EOF
 )"
-OMB_STUB="$(cat \
-<<'EOF'
+OMB_STUB="$(
+    cat \
+        <<'EOF'
 #!/usr/bin/env bash
 if [ -t 1 ]; then
     cat $HOME/.oh-my-bash/README.md
@@ -357,12 +361,12 @@ EOF
 
 # Add RC snippet and custom bash prompt
 if [ "${RC_SNIPPET_ALREADY_ADDED}" != "true" ]; then
-    echo "${RC_SNIPPET}" >> /etc/bash.bashrc
-    echo "${CODESPACES_BASH}" >> "${USER_RC_PATH}/.bashrc"
-    echo 'export PROMPT_DIRTRIM=4' >> "${USER_RC_PATH}/.bashrc"
+    echo "${RC_SNIPPET}" >>/etc/bash.bashrc
+    echo "${CODESPACES_BASH}" >>"${USER_RC_PATH}/.bashrc"
+    echo 'export PROMPT_DIRTRIM=4' >>"${USER_RC_PATH}/.bashrc"
     if [ "${USERNAME}" != "root" ]; then
-        echo "${CODESPACES_BASH}" >> "/root/.bashrc"
-        echo 'export PROMPT_DIRTRIM=4' >> "/root/.bashrc"
+        echo "${CODESPACES_BASH}" >>"/root/.bashrc"
+        echo 'export PROMPT_DIRTRIM=4' >>"/root/.bashrc"
     fi
     chown ${USERNAME}:${USERNAME} "${USER_RC_PATH}/.bashrc"
     RC_SNIPPET_ALREADY_ADDED="true"
@@ -371,12 +375,12 @@ fi
 # Add stub for Oh My Bash!
 if [ ! -d "${USER_RC_PATH}/.oh-my-bash}" ] && [ "${INSTALL_OH_MYS}" = "true" ]; then
     mkdir -p "${USER_RC_PATH}/.oh-my-bash" "/root/.oh-my-bash"
-    echo "${OMB_README}" >> "${USER_RC_PATH}/.oh-my-bash/README.md"
-    echo "${OMB_STUB}" >> "${USER_RC_PATH}/.oh-my-bash/oh-my-bash.sh"
+    echo "${OMB_README}" >>"${USER_RC_PATH}/.oh-my-bash/README.md"
+    echo "${OMB_STUB}" >>"${USER_RC_PATH}/.oh-my-bash/oh-my-bash.sh"
     chmod +x "${USER_RC_PATH}/.oh-my-bash/oh-my-bash.sh"
     if [ "${USERNAME}" != "root" ]; then
-        echo "${OMB_README}" >> "/root/.oh-my-bash/README.md"
-        echo "${OMB_STUB}" >> "/root/.oh-my-bash/oh-my-bash.sh"
+        echo "${OMB_README}" >>"/root/.oh-my-bash/README.md"
+        echo "${OMB_STUB}" >>"/root/.oh-my-bash/oh-my-bash.sh"
         chmod +x "/root/.oh-my-bash/oh-my-bash.sh"
     fi
     chown -R "${USERNAME}:${USERNAME}" "${USER_RC_PATH}/.oh-my-bash"
@@ -384,12 +388,12 @@ fi
 
 # Optionally install and configure zsh and Oh My Zsh!
 if [ "${INSTALL_ZSH}" = "true" ]; then
-    if ! type zsh > /dev/null 2>&1; then
+    if ! type zsh >/dev/null 2>&1; then
         apt-get-update-if-needed
         apt-get install -y zsh
     fi
     if [ "${ZSH_ALREADY_INSTALLED}" != "true" ]; then
-        echo "${RC_SNIPPET}" >> /etc/zsh/zshrc
+        echo "${RC_SNIPPET}" >>/etc/zsh/zshrc
         ZSH_ALREADY_INSTALLED="true"
     fi
 
@@ -408,11 +412,11 @@ if [ "${INSTALL_ZSH}" = "true" ]; then
             -c fetch.fsck.zeroPaddedFilemode=ignore \
             -c receive.fsck.zeroPaddedFilemode=ignore \
             "https://github.com/ohmyzsh/ohmyzsh" "${OH_MY_INSTALL_DIR}" 2>&1
-        echo -e "$(cat "${TEMPLATE_PATH}")\nDISABLE_AUTO_UPDATE=true\nDISABLE_UPDATE_PROMPT=true" > ${USER_RC_FILE}
+        echo -e "$(cat "${TEMPLATE_PATH}")\nDISABLE_AUTO_UPDATE=true\nDISABLE_UPDATE_PROMPT=true" >${USER_RC_FILE}
         sed -i -e 's/ZSH_THEME=.*/ZSH_THEME="codespaces"/g' ${USER_RC_FILE}
 
         mkdir -p ${OH_MY_INSTALL_DIR}/custom/themes
-        echo "${CODESPACES_ZSH}" > "${OH_MY_INSTALL_DIR}/custom/themes/codespaces.zsh-theme"
+        echo "${CODESPACES_ZSH}" >"${OH_MY_INSTALL_DIR}/custom/themes/codespaces.zsh-theme"
         # Shrink git while still enabling updates
         cd "${OH_MY_INSTALL_DIR}"
         git repack -a -d -f --depth=1 --window=1
@@ -425,7 +429,8 @@ if [ "${INSTALL_ZSH}" = "true" ]; then
 fi
 
 # Persist image metadata info, script if meta.env found in same directory
-META_INFO_SCRIPT="$(cat << 'EOF'
+META_INFO_SCRIPT="$(
+    cat <<'EOF'
 #!/bin/sh
 . /usr/local/etc/vscode-dev-containers/meta.env
 
@@ -458,7 +463,7 @@ EOF
 if [ -f "${SCRIPT_DIR}/meta.env" ]; then
     mkdir -p /usr/local/etc/vscode-dev-containers/
     cp -f "${SCRIPT_DIR}/meta.env" /usr/local/etc/vscode-dev-containers/meta.env
-     echo "${META_INFO_SCRIPT}" > /usr/local/bin/devcontainer-info
+    echo "${META_INFO_SCRIPT}" >/usr/local/bin/devcontainer-info
     chmod +x /usr/local/bin/devcontainer-info
 fi
 
@@ -469,6 +474,6 @@ echo -e "\
     LOCALE_ALREADY_SET=${LOCALE_ALREADY_SET}\n\
     EXISTING_NON_ROOT_USER=${EXISTING_NON_ROOT_USER}\n\
     RC_SNIPPET_ALREADY_ADDED=${RC_SNIPPET_ALREADY_ADDED}\n\
-    ZSH_ALREADY_INSTALLED=${ZSH_ALREADY_INSTALLED}" > "${MARKER_FILE}"
+    ZSH_ALREADY_INSTALLED=${ZSH_ALREADY_INSTALLED}" >"${MARKER_FILE}"
 
 echo "Done!"
